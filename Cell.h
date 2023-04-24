58037cabb5a4af9c2f4462cdded4cb3393ed284c
nagorik <fernando.valenzuela01@alumnos.ucn.cl>
2023-04-23 20:08:11 -0400

    Feature: binary registry count record fixed.

--- Cell.h
+++ Cell.h
@@ -5,36 +5,35 @@
 #ifndef _CLION__TALLER1_RIQUELMEFRANCISCO_VALENZUELAFERNANDO_CELL_H
 #define _CLION__TALLER1_RIQUELMEFRANCISCO_VALENZUELAFERNANDO_CELL_H
 
-
 class Cell {
+public:
+    // Constructor por defecto
+    Cell();
+
+    // Constructor con parámetros
+    Cell(int row, int col, bool isAlive = false, int neighborCount = 0);
+
+    // Métodos para obtener y establecer el estado de la célula
+    bool getIsAlive() const;
+    bool setIsAlive(bool status);
+
+    // Métodos para obtener y establecer la posición de la célula
+    int getRow() const;
+    void setRow(int row);
+    int getCol() const;
+    void setCol(int col);
+
+    // Métodos para obtener y establecer el número de vecinos de la célula
+    int getNeighborCount() const;
+    void setNeighborCount(int neighborCount);
+
 private:
     int row;
     int col;
+    bool isAlive;
     int neighborCount;
-    bool isAlive = false;
-public:
-    int getRow() const;
-
-    void setRow(int row);
-
-    int getCol() const;
-
-    void setCol(int col);
-
-    bool isAlive2() const;
-
-    Cell(int row, int col,int neighborCount, bool isAlive);
-
-    int getNeighborCount() const;
-
-    void setNeighborCount(int neighborCount);
-
-    bool isAlive1() const;
-
-    Cell();
-    bool getIsAlive();
-    bool setIsAlive(bool isAlive);
 };
+
 
 
 #endif //_CLION__TALLER1_RIQUELMEFRANCISCO_VALENZUELAFERNANDO_CELL_H
